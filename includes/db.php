c2ac737794ac7e2b7ed75917bc6d18f2ea8091a8
382373-ui <382373@guhsd.net>
2026-04-17 02:56:13 +0000

    fixed the connection, Back To MYSQL

--- includes/db.php
+++ includes/db.php
@@ -3,11 +3,10 @@ require_once 'config.php';
 
 function getDBConnection() {
     try {
-        $dsn = "pgsql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";options='--client_encoding=UTF8'";
+        $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
         ];
         return new PDO($dsn, DB_USER, DB_PASS, $options);
     } catch (PDOException $e) {
